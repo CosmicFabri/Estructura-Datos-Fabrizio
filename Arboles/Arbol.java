8f05e6e9ad5e247b1b9d68e056955ceda8385c0a
CosmicFabri <fabrizzio86@outlook.es>
2023-11-10 10:59:22 -0600

    Codigo documentado

--- Arboles/Arbol.java
+++ Arboles/Arbol.java
@@ -1,24 +1,47 @@
+/**
+ * Clase Arbol. Este contiene nodos, y se debe determinar si es binario o no.
+ */
 public class Arbol<T> {
-    // Declaramos el nodo 'cima' o 'raíz', de un tipo de dato cualquiera
+    /**
+     * Tipo de dato genérico.
+     */
     private NodoArbol<T> raiz;
+    /**
+     * Asignar 'false' para crear un árbol binario.
+     */
     private boolean esBinario = false;
 
-    // Constructor de la clase
+    /**
+     * Crear nuevo árbol y determinar si es binario o no.
+     * @param dato
+     * @param esBinario
+     */
     public Arbol(T dato, boolean esBinario) {
         raiz = new NodoArbol<T>(dato);
         raiz.setEsBinario(esBinario);
         this.esBinario = esBinario;
     }
 
-    // Método para retornar el primer nodo del árbol
+    /**
+     * Devuelve el nodo raíz del arbol.
+     */
     public NodoArbol<T> obtenerRaiz() {
         return raiz;
     }
 
+    /**
+     * Devuelve si el árbol es binario o no, con un valor lógico.
+     * @return
+     */
     public boolean esBinario() {
         return esBinario;
     }
 
+    /**
+     * Agregar un nodo del mismo tipo que su árbol (general o binario).
+     * @param nodoRaiz
+     * @param nodoAgregado
+     */
     public void agregarNodoArbol(NodoArbol<T> nodoRaiz, NodoArbol<T> nodoAgregado) {
         if (esBinario) {
             nodoAgregado.setEsBinario(esBinario);
@@ -27,6 +50,11 @@ public class Arbol<T> {
         nodoRaiz.agregarHijo(nodoAgregado);
     }
 
+    /**
+     * Imprimir el árbol en forma de lista. El usuario puede elegir 
+     * imprimirlo en modo prefijo infijo o posfijo.
+     * @param recorrido
+     */
     public void imprimirArbol(Recorrido recorrido) {
         switch (recorrido.ordinal()) {
             case 0:
--- Arboles/NodoArbol.java
+++ Arboles/NodoArbol.java
@@ -1,90 +1,103 @@
 public class NodoArbol<T> {
-    private T dato;
-    private boolean esBinario = false;
-    private NodoArbol<T> primerHijo;
-    private NodoArbol<T> siguienteHermano;
+	// El nodo es una variable genérica
+	private T dato;
+	// Por defecto, el nodo es general
+	private boolean esBinario = false;
+	// Sólo necesitamos el primer hijo de un nodo para hallar
+	// sus demás hijos, es decir, los hermanos de primer hijo
+	private NodoArbol<T> primerHijo;
+	private NodoArbol<T> siguienteHermano;
 
-    public NodoArbol(T dato) {
-        this.dato = dato;
-        primerHijo = null;
-        siguienteHermano = null;
-    }
+	/**
+	 * Constructor del nodo. Recibe un parámetro de tipo de dato genérico.
+	 * @param dato
+	 */
+	public NodoArbol(T dato) {
+		this.dato = dato;
+		primerHijo = null;
+		siguienteHermano = null;
+	}
 
-    public void agregarHijo(NodoArbol<T> hijo) {
-        if (primerHijo == null) {
-            primerHijo = hijo;
-        } else {
-            NodoArbol<T> hermano = this.primerHijo;
+	public void agregarHijo(NodoArbol<T> hijo) {
+		if (primerHijo == null) {
+			primerHijo = hijo;
+		} else {
+			NodoArbol<T> hermano = this.primerHijo;
+			
+			if (esBinario) {
+				// Debido a la naturaleza del árbol binario,
+				// cada padre puede tener un máximo de 2 hijos
+				if (hermano.siguienteHermano != null) {
+					System.err.println("El nodo ya tiene un hijo, no se debe agregar " + hijo.getDato());
+				} else {
+					hermano.siguienteHermano = hijo;
+				}
+			} else {
+				// Si hay un nodo después del primer hijo, éste
+				// será el hermano anterior al hijo agregado
+				while (hermano.siguienteHermano != null) {
+					hermano = hermano.siguienteHermano;
+				}
+				// Agregar al nuevo hijo al lado del último hermano
+				hermano.siguienteHermano = hijo;
+			}
+		}
+	}
 
-            if (esBinario) {
-                if (hermano.siguienteHermano != null) {
-                    System.err.println("El nodo ya tiene un hijo, no se debe agregar " + hijo.getDato());
-                } else {
-                    hermano.siguienteHermano = hijo;
-                }
-            } else {
-                while (hermano.siguienteHermano != null) {
-                    hermano = hermano.siguienteHermano;
-                }
-                hermano.siguienteHermano = hijo;
-            }
-        }
-    }
+	public NodoArbol<T> obtenerPrimerHijo() {
+		return primerHijo;
+	}
 
-    public NodoArbol<T> obtenerPrimerHijo() {
-        return primerHijo;
-    }
+	public NodoArbol<T> obtenerSiguienteHermano() {
+		return siguienteHermano;
+	}
 
-    public NodoArbol<T> obtenerSiguienteHermano() {
-        return siguienteHermano;
-    }
+	public T getDato() {
+		return dato;
+	}
 
-    public T getDato() {
-        return dato;
-    }
+	public boolean esBinario() {
+		return esBinario;
+	}
 
-    public boolean esBinario() {
-        return esBinario;
-    }
+	public void setEsBinario(boolean esBinario) {
+		this.esBinario = esBinario;
+	}
 
-    public void setEsBinario(boolean esBinario) {
-        this.esBinario = esBinario;
-    }
+	public void imprimirEnPrefijo() {
+		System.out.print(dato + "\t");
+		NodoArbol<T> hijo = primerHijo;
 
-    public void imprimirEnPrefijo() {
-        System.out.print(dato + "\t");
-        NodoArbol<T> hijo = primerHijo;
+		while (hijo != null) {
+			hijo.imprimirEnPrefijo();
+			hijo = hijo.siguienteHermano;
+		}
+	}
 
-        while (hijo != null) {
-            hijo.imprimirEnPrefijo();
-            hijo = hijo.siguienteHermano;
-        }
-    }
+	public void imprimirEnInfijo() {
+		NodoArbol<T> hijo = primerHijo;
 
-    public void imprimirEnInfijo() {
-        NodoArbol<T> hijo = primerHijo;
+		if (hijo != null) {
+			hijo.imprimirEnInfijo();
+		}
 
-        if (hijo != null) {
-            hijo.imprimirEnInfijo();
-        }
+		System.out.print(dato + "\t");
+		while (hijo != null) {
+			hijo = hijo.siguienteHermano;
+			if (hijo != null) {
+				hijo.imprimirEnInfijo();
+			}
+		}
+	}
 
-        System.out.print(dato + "\t");
-        while (hijo != null) {
-            hijo = hijo.siguienteHermano;
-            if (hijo != null) {
-                hijo.imprimirEnInfijo();
-            }
-        }
-    }
+	public void imprimirEnPosfijo() {
+		NodoArbol<T> hijo = primerHijo;
 
-    public void imprimirEnPosfijo() {
-        NodoArbol<T> hijo = primerHijo;
+		while (hijo != null) {
+			hijo.imprimirEnPosfijo();
+			hijo = hijo.siguienteHermano;
+		}
 
-        while (hijo != null) {
-            hijo.imprimirEnPosfijo();
-            hijo = hijo.siguienteHermano;
-        }
-
-        System.out.print(dato + "\t");
-    }
+		System.out.print(dato + "\t");
+	}
 }
