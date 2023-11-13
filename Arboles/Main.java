9906f60f4d92947f4538fc68d26027fb43433a7d
CosmicFabri <fabrizzio86@outlook.es>
2023-11-12 19:26:14 -0600

    Archivos añadidos

--- Arboles/Main.java
+++ Arboles/Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
         // Creando el árbol ordinario desde la raíz
-        Arbol<Character> arbolPrincipal = new Arbol<>('A', false);
+        Arbol<Character> arbolPrincipal = new Arbol<Character>('A', false);
         NodoArbol<Character> raiz = arbolPrincipal.obtenerRaiz();
 
         // Añadiendo el segundo nivel del árbol
-        NodoArbol<Character> nodoB = new NodoArbol<>('B');
+        NodoArbol<Character> nodoB = new NodoArbol<Character>('B');
         arbolPrincipal.agregarNodoArbol(raiz, nodoB);
 
         // Imprimiendo el árbol
